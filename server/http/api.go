ea793eba25a5ee6ad23084ce3910043293a41b83
9ssi7 <info@ssibrahimbas.com>
2024-01-19 01:05:55 +0300

    implement the http app for first time

--- server/http/api.go
+++ server/http/api.go
@@ -1,1 +1,24 @@
 package http
+
+import (
+	"github.com/cilloparch/cillop/middlewares/i18n"
+	"github.com/cilloparch/cillop/result"
+	"github.com/gofiber/fiber/v2"
+	"github.com/turistikrota/service.help/app/query"
+	"github.com/turistikrota/service.help/domains/faq"
+)
+
+func (h srv) FaqFilter(ctx *fiber.Ctx) error {
+	filter := faq.FilterEntity{}
+	h.parseQuery(ctx, &filter)
+	l, a := i18n.GetLanguagesInContext(*h.i18n, ctx)
+	filter.Locale = l
+	query := query.FaqFilterQuery{
+		FilterEntity: &filter,
+	}
+	res, err := h.app.Queries.FaqFilter(ctx.UserContext(), query)
+	if err != nil {
+		return result.Error(h.i18n.TranslateFromError(*err, l, a))
+	}
+	return result.SuccessDetail(Messages.Success.Ok, res)
+}
