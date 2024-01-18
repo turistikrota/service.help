1b19654098c8fb3afc721ec9f92f8e554e137a59
9ssi7 <info@ssibrahimbas.com>
2024-01-19 00:57:08 +0300

    add faq filter query

--- app/query/faq_filter.go
+++ app/query/faq_filter.go
@@ -1,1 +1,33 @@
 package query
+
+import (
+	"context"
+
+	"github.com/cilloparch/cillop/cqrs"
+	"github.com/cilloparch/cillop/i18np"
+	"github.com/turistikrota/service.help/domains/faq"
+)
+
+type FaqFilterQuery struct {
+	*faq.FilterEntity
+}
+
+type FaqFilterRes struct {
+	List []faq.ListDto
+}
+
+type FaqFilterHandler cqrs.HandlerFunc[FaqFilterQuery, *FaqFilterRes]
+
+func NewFaqFilterHandler(repo faq.Repo) FaqFilterHandler {
+	return func(ctx context.Context, q FaqFilterQuery) (*FaqFilterRes, *i18np.Error) {
+		list, err := repo.Filter(ctx, *q.FilterEntity)
+		if err != nil {
+			return nil, err
+		}
+		res := make([]faq.ListDto, len(list))
+		for i, v := range list {
+			res[i] = v.ToList()
+		}
+		return &FaqFilterRes{List: res}, nil
+	}
+}
